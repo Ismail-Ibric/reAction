3004a073228cda2bf03b0912656036ab10f4ec9f
Ismail Ibric <iibric@hotmail.com>
2021-11-29 13:50:41 -0500

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# reAction
+# reAction REST API
+
+![Imgur](https://i.imgur.com/g4mowgX.png)
+
+## Details
+
+Watches for GitHub Actions to rebuild a repo.
+
+## Frameworks / Libraries
+
+- Node.js
+  -
+  - TypeScript used for strict typing.
+  - Express used to serve endpoints.
+  - Spawning a process, waiting, reading output.
+  - GitHub CLI called to clone a Repo, with a Private Key.
+  - ...
--- src/rebuild.ts
+++ src/rebuild.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import { spawn } from "child_process";
+import fs from "fs";
+import path from "path";
 
 const rebuild = (req: Request, res: Response) => {
-
+  
   let proj = req.body?.project;
+  const host = req.get('host');
+  const addr = req.socket.remoteAddress;
+
+  console.log(`request orginated from '${host}' - '${addr}'`);
 
   switch( proj ) {
     case "Portfolio": 
@@ -16,14 +22,27 @@ const rebuild = (req: Request, res: Response) => {
 
 }
 
+
 const rebuildPortfolio = (res: Response) => {
   const loc = "Ismail-Ibric/Portfolio";
-  const spawned = spawn('/ghcli/bin/gh',
+  const repoDir = "/tmp/Portfolio";
+  const destDir = "/home/izzysoft/www";
+
+  // delete directory recursively
+  deleteDir(repoDir);
+
+  const spawned = spawn('gh',
     ['repo','clone',loc],
     {cwd:'/tmp'});
 
+  let isCloningInto = false;
+
   spawned.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
+    isCloningInto = data.indexOf("Cloning into '", 0) === 0;
+    if( isCloningInto )
+      console.log(`stdout: ${data}`);
+    else
+      console.error(`stderr: ${data}`);
   });
   
   spawned.on('error', (error) => {
@@ -35,9 +54,42 @@ const rebuildPortfolio = (res: Response) => {
   });
 
   spawned.on('close', (code) => {
+    if( isCloningInto )
+      code = 1;
+    
     console.log(`child process exited with code ${code}`);
-    return res.status(201).send("SUCCESS");
+    copyDir(repoDir+"/build", destDir);
+    if( code === 1 || isCloningInto )
+      return res.status(201).send("SUCCESS");
+    else
+      return res.status(500).send("ERROR");
   });
 }
+
+
+async function copyDir(src: string, dest: string) {
+  await fs.promises.mkdir(dest, { recursive: true });
+  let entries = await fs.promises.readdir(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+      let srcPath = path.join(src, entry.name);
+      let destPath = path.join(dest, entry.name);
+
+      entry.isDirectory() ?
+          await copyDir(srcPath, destPath) :
+          await fs.promises.copyFile(srcPath, destPath);
+  }
+}
+
+
+async function deleteDir( repoDir: string ) {
+  await fs.promises.rm(repoDir, {recursive: true, force: true})
+  .then(() => {
+    console.log(`${repoDir} was removed!`);
+  }).catch((error: any) => {
+    console.error(error.message);
+  });
+}
+
 
 export default rebuild;
